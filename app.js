ac801df3f15d8c92ace35b5a2acb3015356206c8
Kanha gupta <kanhag4163@gmail.com>
2022-09-10 13:47:18 +0530

    -

--- app.js
+++ app.js
@@ -2,38 +2,49 @@ const fs = require("fs");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
+const {response} = require("express");
+const {contentDisposition} = require("express/lib/utils");
 
+const app = express();
 app.use(express.static("views"));
 app.use(express.urlencoded({ extended: true }));
+
+
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
-app.get("/confirm", function (req, res) {
-  res.sendFile(__dirname + "/views/confirm.html");
+app.get("/studentlist", function (req, res) {
+  res.sendFile(__dirname + "/Studentlist.html");
+
 });
-app.get("/about", function (req, res) {
-  res.sendFile(__dirname + "/views/about.html");
-});
-app.get("/recommend", function (req, res) {
-  res.sendFile(__dirname + "/views/recommend.html");
-});
-app.post("/recommend", function (req, res) {
-  const restaurantData = req.body;     //we can also do req.body.name but that will be lengthy so will get the full bofy og recommend.html 
+app.get("/data/recommend.json", function (req,res) {
+  res.sendFile(__dirname + "/data/recommend.json")
+})
+app.post("/", function (req, res) {
+  const Studentdata = req.body;     //we can also do req.body.name but that will be lengthy so will get the full bofy og
   const filePath = __dirname+"/data/recommend.json";  //path of the file
   const fileData = fs.readFileSync(filePath);  //reading file path to get existing data
-  const storedResaturants = JSON.parse(fileData);   //parsing the data (because before it's only a text technically)
-  storedResaturants.push(restaurantData)    //adding new data by .push
-  fs.writeFileSync(filePath,JSON.stringify(storedResaturants))  //now writing that data 
-
-  res.redirect("/confirm")
-  console.log(restaurantData);
+  const StoredData = JSON.parse(fileData);   //parsing the data (because before it's only a text technically)
+  StoredData.push(Studentdata)    //adding new data by .push
+  fs.writeFileSync(filePath,JSON.stringify(StoredData))  //now writing that data
+  res.redirect("/Studentlist")
+  console.log(Studentdata);
 });
-app.get("/restaurants", function (req, res) {
-  res.sendFile(__dirname + "/views/restaurants.html");
-});
-
 app.listen(3000, function () {
   console.log("server is runnin at port 3000");
 });
+
+//below code to fetch api
+// function getData() {
+//     url="/data/recommend.json"
+//     fetch(url).then((response)=>{
+//         return response.json();
+//     }).then((data)=>{
+//         console.log(data)
+//     })
+// }
+//
+// console.log("before running getdata")
+// getData()
+// console.log("After running getData")
